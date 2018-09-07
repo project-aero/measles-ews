b4c751e89b57785607edc8ab2596bb02811a8b41
Andrew Tredennick <atredenn@gmail.com>
2018-09-07 09:55:14 -0400

    adds prelim initial pfilter search results and plot

--- code/initial-pfilter-job.sh
+++ code/initial-pfilter-job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -t 20:00
+#SBATCH -t 10:00:00
 #SBATCH -N 1
-#SBATCH --ntasks-per-node 20
+#SBATCH --ntasks-per-node 21
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
  
-cd $SBATCH_O_WORKDIR
+cd ~/measles/code/
 module load R
  
 # parallel R: submit job with one MPI master
