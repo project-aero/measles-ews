5a1e62166a1bf53e9de06b7c23f3042011ee5d3b
Andrew Tredennick <atredenn@gmail.com>
2018-10-19 13:50:15 -0400

    implements MCAP procedure for ci estimation via profile likelihood

--- code/analyze-likelihood-profiles.R
+++ code/analyze-likelihood-profiles.R
@@ -1,5 +1,44 @@
 # analyze-likelihood-profiles.R
 
+mcap <- function(lp,parameter,confidence=0.95,lambda=0.75,Ngrid=1000)
+{
+  smooth_fit <- loess(lp ~ parameter,span=lambda)
+  parameter_grid <- seq(min(parameter), max(parameter), length.out = Ngrid)
+  smoothed_loglik <- predict(smooth_fit,newdata=parameter_grid)
+  smooth_arg_max <- parameter_grid[which.max(smoothed_loglik)]
+  dist <- abs(parameter-smooth_arg_max)
+  included <- dist < sort(dist)[trunc(lambda*length(dist))]
+  maxdist <- max(dist[included])
+  weight <- rep(0,length(parameter))
+  weight[included] <- (1-(dist[included]/maxdist)^3)^3
+  quadratic_fit <- lm(lp ~ a + b, weight=weight,
+                      data = data.frame(lp=lp,b=parameter,a=-parameter^2)
+  )
+  b <- unname(coef(quadratic_fit)["b"] )
+  a <- unname(coef(quadratic_fit)["a"] )
+  m <- vcov(quadratic_fit)
+  var_b <- m["b","b"]
+  var_a <- m["a","a"]
+  cov_ab <- m["a","b"]
+  se_mc_squared <- (1 / (4 * a^2)) * (var_b - (2 * b/a) * cov_ab + (b^2 / a^2) * var_a)
+  se_stat_squared <- 1/(2*a)
+  se_total_squared <- se_mc_squared + se_stat_squared
+  delta <- qchisq(confidence,df=1) * ( a * se_mc_squared + 0.5)
+  loglik_diff <- max(smoothed_loglik) - smoothed_loglik
+  ci <- range(parameter_grid[loglik_diff < delta])
+  list(lp=lp,parameter=parameter,confidence=confidence,
+       quadratic_fit=quadratic_fit, quadratic_max=b/(2*a),
+       smooth_fit=smooth_fit,
+       fit=data.frame(
+         parameter=parameter_grid,
+         smoothed=smoothed_loglik,
+         quadratic=predict(quadratic_fit, list(b = parameter_grid, a = -parameter_grid^2))
+       ),
+       mle=smooth_arg_max, ci=ci, delta=delta,
+       se_stat=sqrt(se_stat_squared), se_mc=sqrt(se_mc_squared), se=sqrt(se_total_squared)
+  )
+}
+
 
 # Load packages -----------------------------------------------------------
 
@@ -9,46 +48,15 @@ library(tidyverse)
 # Load profile results ----------------------------------------------------
 
 profile_data <- read.csv("../results/rho-profile-Niamey.csv") %>%
-  slice(2:n()) %>%  # chop off first NA row
-  filter(loglik > max(loglik) - 20)
+  slice(2:n())  # chop off first NA row
 
-preds <- predict(
-  mgcv::gam(
-    loglik ~ s(rho_value, bs = "cs"), data = profile_data
-  ),
-  newdata = data.frame(
-    rho_value = seq(0.2, 0.54, by = 0.0001)
-  )
-)
+test <- mcap(lp = profile_data$loglik, parameter = profile_data$rho_value)
 
-pred_df <- tibble(
-  rho_value = seq(0.2, 0.54, by = 0.0001),
-  loglik = preds
-)
+ggplot(test$fit, aes(x=parameter)) +
+  geom_point(data = profile_data, aes(x = rho_value, y = loglik), shape = 1, color = "grey75") +
+  geom_line(aes(y = smoothed), color = "red") +
+  geom_line(aes(y = quadratic), color = "blue", linetype = 2) +
+  geom_vline(aes(xintercept = test$ci[1]), color = "red") +
+  geom_vline(aes(xintercept = test$ci[2]), color = "red") +
+  theme_minimal() 
 
-vspots <- pred_df %>%
-  filter(loglik > max(loglik, na.rm = T) - 1.92) %>%
-  pull(rho_value)
-
-vspots2 <- profile_data %>%
-  filter(loglik > max(pred_df$loglik, na.rm = T) - 1.92) %>%
-  pull(rho_value)
-
-cspot <- pred_df %>%
-  filter(loglik == max(loglik, na.rm = T)) %>%
-  mutate(
-    loglik - 1.92
-  ) %>%
-  pull(loglik)
-
-
-ggplot(profile_data, aes(x = rho_value, y = loglik)) +
-  geom_point(shape = 1) +
-  geom_hline(aes(yintercept = max(preds) - 1.92), color = "blue") +
-  geom_line(data = pred_df, aes(x = rho_value, y = loglik), color = "red")+
-  geom_vline(aes(xintercept = min(vspots)), color = "blue") +
-  geom_vline(aes(xintercept = max(vspots)), color = "blue") +
-  geom_vline(aes(xintercept = min(vspots2)), color = "blue", linetype = 2) +
-  geom_vline(aes(xintercept = max(vspots2)), color = "blue", linetype = 2) +
-  labs(x = expression(rho), y = "profile log-likelihood") +
-  theme_minimal()
