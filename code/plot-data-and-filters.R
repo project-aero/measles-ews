1bbb1575efadee2bb17ec568e95738c38f47f4c2
Andrew Tredennick <atredenn@gmail.com>
2019-01-02 17:11:19 -0500

    Adds new results for elimination AUCs over a moving window. Also updates the seasonal R0 plots for each city to show the bootstrap confidence intervals around the seasonal curves.

--- code/plot-data-and-filters.R
+++ code/plot-data-and-filters.R
@@ -141,7 +141,7 @@ r_squares <- pred_cases %>%
   dplyr::select(city_name, R2)
 
 scatters <- ggplot(pred_cases, aes(x = log(observed_cases+1), y = log(mean_cases+1))) +
-  geom_point(color = "dodgerblue4", size = 1, alpha = 0.3) +
+  geom_point(aes(color = city_name), size = 1, alpha = 0.3) +
   geom_abline(aes(intercept = 0, slope = 1), linetype = 2) +
   geom_label(data = r_squares, 
              aes(x = 1.7, y = 7.3, label = paste0("italic(R)^2 == ", 
@@ -151,23 +151,109 @@ scatters <- ggplot(pred_cases, aes(x = log(observed_cases+1), y = log(mean_cases
   facet_wrap(~city_name, nrow = 2, scales = "free") +
   scale_y_continuous(limits = c(0,8)) +
   scale_x_continuous(limits = c(0,8)) +
+  scale_color_colorblind()+
+  guides(color = FALSE)+
   theme_classic() +
   theme(panel.spacing = unit(1, "lines"), strip.background = element_blank(),
-        strip.text = element_text(face = "bold"))
+        strip.text = element_text(face = "bold")) +
+  ggtitle("A. Model-data agreement")
 # ggsave(filename = "../figures/pred-obs-scatters.pdf", plot = scatters, width = 8.5, height = 2.5, units = "in")
 
 
 # Plot R0 curves ----------------------------------------------------------
 
 # Define function to calculate R0 from seasonal params
-calc_R0 <- function(beta, qis, season, eta = (365/8), 
-                    mu = 0.05, nu = 0.05, gamma = (365/5)){
-  B <- as.numeric((1 + exp(season %*% qis)) * beta)
-  R0 <- (eta*B*mu) / (nu*(eta + nu)*(gamma + nu))
+calc_R0 <- function(beta = NULL, B = NULL, qis = NULL, season = NULL, 
+                    eta = (365/8), mu = 0.05, nu = 0.05, gamma = (365/5)){
+  if(is.null(beta)){
+    R0 <- (eta*B*mu) / (nu*(eta + nu)*(gamma + nu))
+  } else{
+    B <- as.numeric((1 + exp(season %*% qis)) * beta)
+    R0 <- (eta*B*mu) / (nu*(eta + nu)*(gamma + nu))
+  }
   return(R0)
 }
 
-# Calculate R0 curves for each city
+# Define computation grid for bootstraps
+
+nboots <- 100
+nmifs <- 50
+comp_grid <- expand.grid(1:nboots, 1:50)
+colnames(comp_grid) <- c("boot_series", "param_set")
+comp_grid$do_grid <- 1:nrow(comp_grid)
+
+# Load example pomp file for basis function
+pomp_file <- "../code/measles-pomp-object-Agadez.RDS"
+measles_pomp <- readRDS(pomp_file)  # exemplar bases
+
+bases <- as_tibble(measles_pomp@covar) %>%
+  dplyr::select(starts_with("x")) %>%
+  dplyr::slice(1:365) %>%
+  mutate(
+    day = 1:365
+  ) %>%
+  gather(key = base, value = value, -day)
+
+season <- bases %>%
+  spread(key = base, value = value) %>%
+  dplyr::select(-day) %>%
+  as.matrix()
+
+seasonal_functions <- tibble()
+for(do_city in c("Agadez", "Maradi", "Niamey", "Zinder")){
+  boots <- read.csv(paste0("../results/bootstrap-mif-lls-", do_city, ".csv"))
+  
+  b_splines <- boots %>%
+    slice(2:n()) %>%  # ignore first row of NAs
+    left_join(comp_grid, by = "do_grid") %>%  # merge in comp grid info
+    group_by(boot_series) %>%
+    filter(loglik == max(loglik)) %>%
+    ungroup() %>%
+    dplyr::select(b1, b2, b3, b4, b5, b6)
+  
+  betas <- boots %>%
+    slice(2:n()) %>%  # ignore first row of NAs
+    left_join(comp_grid, by = "do_grid") %>%  # merge in comp grid info
+    group_by(boot_series) %>%
+    filter(loglik == max(loglik)) %>%
+    ungroup() %>%
+    dplyr::select(beta_mu)
+  
+  seasonal_betas <- tibble()
+  for(i in 1:nrow(b_splines)){
+    qis <- as.numeric(b_splines[i, ])
+    beta_tmp <- as.numeric(betas[i, "beta_mu"])
+    
+    seasonal_tmp <- tibble(
+      beta = as.numeric((1+exp(season %*% qis)) * beta_tmp),
+      day = 1:365,
+      boot = i
+    )
+    seasonal_betas <- bind_rows(seasonal_betas, seasonal_tmp)
+  }  # end bootstrap loop
+  
+  seasonal_betas <- seasonal_betas %>%
+    mutate(
+      city = do_city,
+      rnaught = calc_R0(B = beta)
+    ) %>%
+    group_by(city, day) %>%
+    summarise(
+      upper_R0 = quantile(rnaught, 0.975),
+      lower_R0 = quantile(rnaught, 0.025)
+    ) %>%
+    ungroup()
+  
+  seasonal_functions <- bind_rows(seasonal_functions, seasonal_betas)
+  
+}  # end city loop
+
+seasonal_functions <- seasonal_functions %>%
+  mutate(
+    date = as.Date(day, origin = "2016-12-31",tz = "UTC")
+  )
+
+# Calculate MLE R0 curves for each city
 
 R_0s <- tibble()
 
@@ -175,6 +261,7 @@ for(do_city in c("Agadez", "Maradi", "Niamey", "Zinder")){
   
   # Load fitted parameters and pomp model 
   mle_file <- paste0("../results/initial-mif-lls-", do_city, ".csv")
+  
   mles <- read.csv(mle_file) %>% 
     slice(2:n()) %>%  # ignore first row of storage NAs
     filter(loglik == max(loglik, na.rm = TRUE)) %>%
@@ -210,7 +297,7 @@ for(do_city in c("Agadez", "Maradi", "Niamey", "Zinder")){
   
   tmp_out <- tibble(
     city = do_city,
-    doy = 1:length(R0),
+    day = 1:length(R0),
     R0 = R0
   )
   
@@ -219,18 +306,38 @@ for(do_city in c("Agadez", "Maradi", "Niamey", "Zinder")){
 
 R_0s <- R_0s %>%
   mutate(
-    date = as.Date(doy, origin = "2016-12-31",tz = "UTC")
+    date = as.Date(day, origin = "2016-12-31",tz = "UTC")
   )
 
-rnaughts <- ggplot(R_0s, aes(x = date, y = R0, color = city)) +
-  geom_line() +
-  scale_color_colorblind(name = NULL) +
-  scale_x_date(date_labels = "%b", date_breaks = "2 months") +
-  labs(x = "Time of year", y = expression(R[0])) +
-  theme_classic() +
-  theme(legend.position = c(0.8, 0.9), 
-        legend.box.background = element_blank(),
-        legend.key.size = unit(0.8, 'lines'))
+all_R_0s <- left_join(R_0s, seasonal_functions, by = c("city", "day", "date"))
 
-scatters_and_season <- cowplot::plot_grid(scatters, rnaughts, labels = "AUTO", scale = 0.9, label_size = 12)
-ggsave(filename = "../figures/pred-obs-scatters.pdf", plot = scatters_and_season, width = 8.5, height = 4, units = "in")
+rnaughts <- ggplot(all_R_0s, aes(x = date)) +
+  geom_ribbon(aes(ymin = lower_R0, ymax = upper_R0, fill = city), alpha = 0.6) +
+  geom_line(aes(y = R0), color = "white") +
+  scale_fill_colorblind(name = NULL) +
+  scale_color_colorblind(name = NULL) +
+  guides(fill = FALSE) +
+  facet_wrap(~city,nrow = 2, scales = "free") +
+  labs(x = "Time of year", y = expression(R[0])) +
+  scale_x_date(date_labels = "%b", date_breaks = "2 months") +
+  scale_y_continuous(limits = c(0,35))+
+  theme_classic() +
+  theme(panel.spacing = unit(1, "lines"), strip.background = element_blank(),
+        strip.text = element_text(face = "bold")) +
+  ggtitle("B. Seasonal basic reproduction number")
+
+
+# rnaughts <- ggplot(R_0s, aes(x = date, y = R0, color = city)) +
+#   geom_line() +
+#   scale_color_colorblind(name = NULL) +
+#   scale_x_date(date_labels = "%b", date_breaks = "2 months") +
+#   labs(x = "Time of year", y = expression(R[0])) +
+#   theme_classic() +
+#   theme(legend.position = c(0.8, 0.9), 
+#         legend.box.background = element_blank(),
+#         legend.key.size = unit(0.8, 'lines'))
+
+scatters_and_season <- cowplot::plot_grid(scatters, rnaughts, labels = NULL, scale = 0.9, label_size = 12)
+ggsave(filename = "../figures/pred-obs-scatters.pdf", plot = scatters_and_season, width = 9.5, height = 5, units = "in")
+
+
